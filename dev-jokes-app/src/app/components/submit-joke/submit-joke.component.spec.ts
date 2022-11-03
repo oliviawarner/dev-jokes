ac7cd546fef548b19c18140ddd22a9c9b0adf57c
owarner <owarner@ceiamerica.com>
2022-11-03 12:55:52 -0400

    added unit testing

--- dev-jokes-app/src/app/components/submit-joke/submit-joke.component.spec.ts
+++ dev-jokes-app/src/app/components/submit-joke/submit-joke.component.spec.ts
@@ -28,4 +28,37 @@ describe('SubmitJokeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('initial form values should equal group', () => {
+    const jokeFormGroup = component.jokeForm;
+    const jokeFormValues = {
+      name: '',
+      twitter:'',
+      question:'',
+      punchline: ''
+    }
+    expect(jokeFormGroup.value).toEqual(jokeFormValues);
+  });
+
+  it('form group should have 2 input elements', () => {
+    const formElement = fixture.debugElement.nativeElement.querySelector('#jokeForm');
+    const inputElements = formElement.querySelectorAll('input');
+    expect(inputElements.length).toEqual(2);
+  });
+
+  it('form group should have 2 textarea elements', () => {
+    const formElement = fixture.debugElement.nativeElement.querySelector('#jokeForm');
+    const textareaElements = formElement.querySelectorAll('textarea');
+    expect(textareaElements.length).toEqual(2);
+  });
+
+  it('form should be initialized to invalid', () => {
+    expect(component.jokeForm.valid).toBeFalsy();
+  });
+
+  it('submit button should be initially disabled', () => {
+    const submitBtn = fixture.debugElement.nativeElement.querySelector('#submit_joke');
+    expect(submitBtn.attributes.disabled).toBeTruthy();
+  });
+
 });
