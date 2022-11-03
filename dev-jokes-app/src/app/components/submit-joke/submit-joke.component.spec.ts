eb542058ec20b43b72fabcc0e3ad50eb81d41d0b
owarner <owarner@ceiamerica.com>
2022-11-03 16:30:51 -0400

    added matsnack bar import

--- dev-jokes-app/src/app/components/submit-joke/submit-joke.component.spec.ts
+++ dev-jokes-app/src/app/components/submit-joke/submit-joke.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SubmitJokeComponent } from './submit-joke.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('SubmitJokeComponent', () => {
   let component: SubmitJokeComponent;
@@ -12,7 +13,8 @@ describe('SubmitJokeComponent', () => {
       imports:[
         FormsModule,
         ReactiveFormsModule,
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        MatSnackBarModule
       ],
       declarations: [ SubmitJokeComponent ]
     })
